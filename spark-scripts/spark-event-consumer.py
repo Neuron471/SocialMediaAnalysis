d839af2983ad0087debc9036577a353eb202583b
Farah Shadra <113090058+Neuron471@users.noreply.github.com>
2024-02-03 23:43:16 +0700

    Update spark-event-consumer.py

--- spark-scripts/spark-event-consumer.py
+++ spark-scripts/spark-event-consumer.py
@@ -1,47 +1,60 @@
 import pyspark
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col, from_unixtime, date_format
+from pyspark.sql.functions import from_json, col, from_unixtime, expr
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType
 import os
 from dotenv import load_dotenv
 from pathlib import Path
 
+# Load environment variables from the .env file
 dotenv_path = Path("/opt/app/.env")
 load_dotenv(dotenv_path=dotenv_path)
 
+# Retrieve Spark configuration from environment variables
 spark_hostname = os.getenv("SPARK_MASTER_HOST_NAME")
 spark_port = os.getenv("SPARK_MASTER_PORT")
 kafka_host = os.getenv("KAFKA_HOST")
 kafka_topic = os.getenv("KAFKA_TOPIC_NAME")
 
+# PostgreSQL database connection details
 PG_HOST = 'ep-winter-band-34900791.ap-southeast-1.aws.neon.tech'
 PG_DATABASE = 'SocmedDB'
 PG_USER = 'farahduta7'
 PG_PASSWORD = 'Mf9qmk0iKZhU'
 
+# Spark master URL
 spark_host = f"spark://{spark_hostname}:{spark_port}"
 
+# Set Spark submit arguments including required packages
 os.environ[
     "PYSPARK_SUBMIT_ARGS"
 ] = "--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.2 org.postgresql:postgresql:42.2.18"
 
+# Initialize or retrieve SparkContext with appropriate configuration
 sparkcontext = pyspark.SparkContext.getOrCreate(
     conf=(pyspark.SparkConf().setAppName("DibimbingStreaming").setMaster(spark_host))
 )
+
+# Set log level for Spark
 sparkcontext.setLogLevel("WARN")
+
+# Create or retrieve SparkSession
 spark = pyspark.sql.SparkSession(sparkcontext.getOrCreate())
 
-
+# Define the schema for Kafka messages
 kafka_schema = StructType([
     StructField("ID", StringType(), True),
     StructField("Kota", StringType(), True),
     StructField("Sosial_Media", StringType(), True),
     StructField("Usage_Count", IntegerType(), True),
     StructField("Sales", IntegerType(), True),
+    StructField("Visitors", IntegerType(), True),
+    StructField("PageVisitors", IntegerType(), True),
+    StructField("Conversion", IntegerType(), True),
     StructField("Ts", IntegerType(), True),
 ])
 
-
+# Read streaming data from Kafka
 stream_df = (
     spark.readStream.format("kafka")
     .option("kafka.bootstrap.servers", f"{kafka_host}:9092")
@@ -50,16 +63,18 @@ stream_df = (
     .load()
 )
 
+# Parse Kafka messages and apply transformations
 parsed_df = stream_df.select(from_json(col("value").cast("string"), kafka_schema).alias("parsed_value")) \
     .select("parsed_value.*") \
-    .withColumn("Ts", from_unixtime("Ts").cast(TimestampType()))
+    .withColumn("Ts", from_unixtime("Ts").cast(TimestampType())) \
+    .withColumn("ConversionRate", expr("(Conversion/Visitors)*100")) \
+    .withColumn("BounceRate", expr("(PageVisitors/Visitors)*100")) \
+    .withWatermark("Ts", "5 minutes")  # Set watermark with a 5-minute handled late data
 
-
+# Define the function to write to PostgreSQL
 def write_to_postgresql(batch_df, batch_id):
-
     print(f"Processing batch {batch_id}")
     batch_df.show(truncate=False)
-    
 
     batch_df.write \
         .format("jdbc") \
@@ -70,14 +85,14 @@ def write_to_postgresql(batch_df, batch_id):
         .mode("append") \
         .save()
 
-
+# Start the streaming query
 query = (
     parsed_df.writeStream
     .outputMode("append")
     .foreachBatch(write_to_postgresql)
-    .trigger(processingTime="5 seconds")
+    .trigger(processingTime="7 seconds")  # Set processing time to 7 seconds
     .start()
 )
 
-
+# Await termination
 query.awaitTermination()
